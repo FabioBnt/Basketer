99162f2fc391defe921d4cf7db6562b4ba155e08
Ismail <ismailalri@gmail.com>
2022-12-09 11:10:55 +0100

    feat : select players and insert player wip : modif player

--- php/Players.php
+++ php/Players.php
@@ -3,6 +3,68 @@ include './DataBase.php';
 
 class players
 {
+    public function selectPlayers($number='', $familyName='', $name='', $photo='', $birthDate='', $size=0, $weight=0, $prefPos='', $status='')
+    {
+        $numCols = 0;
+        $conds = 'where ';
+        if($number!=''){
+           $conds.+"NumLicence = '$number' AND ";
+           $numCols++;
+        }
+        if($familyName!=''){
+            $conds.+"Nom = '$familyName' AND ";
+            $numCols++;
+        }
+        if($name!=''){
+            $conds.+"Prenom = '$name' AND ";
+            $numCols++;
+        } 
+        if($photo!=''){
+            $conds.+"Photo = '$photo' AND ";
+            $numCols++;
+        }
+        if($birthDate!=''){ 
+            $conds.+"DateNaiss = '$birthDate' AND ";
+            $numCols++;
+        }
+        if($size != 0){
+            $conds.+"Taille = '$size' AND ";
+            $numCols++;
+        }
+        if($weight != 0){
+            $conds.+"Poids = '$weight' AND ";
+            $numCols++;
+        }
+        if($prefPos != ''){
+            $conds.+"Taille = '$size' AND ";
+            $numCols++;
+        }
+        if($status != ''){
+            $conds.+"Status = '$status'    ";
+            $numCols++;
+        }
+        $conds = substr($conds, 0, -4);
+
+        $mysql = DataBase::getInstance();
+        $data = array();
+        if($numCols != 0){
+            $data = $mysql->select('*', 'Joueur', $conds);
+        }else{
+            $data = $mysql->select('*', 'Joueur');
+        }
+        return $data;
+    }
+    public function insertPlayer($number, $familyName, $name, $photo, $birthDate, $size, $weight, $prefPos, $status)
+    {
+        $mysql = DataBase::getInstance();
+        $values = array($number, $familyName, $name, $photo, $birthDate, $size, $weight, $prefPos, $status);
+        $mysql->insert('`Joueur` (`NumLicence`, `Nom`, `Prenom`, `Photo`, `DateNaiss`, `Taille`, `Poids`, `PostePref`, `Statut`)', 8, $values);
+    }
+    public function modifyPlayer(){
+        
+    }
+
+
 
 }
 
