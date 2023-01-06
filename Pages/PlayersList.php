c6487e3a87249367122511f1414362ba55c7e5c8
Ismail <ismailalri@gmail.com>
2023-01-06 10:15:51 +0100

    feat : page list PlayersList.php

---
+++ Pages/PlayersList.php
@@ -0,0 +1,60 @@
+<h1>player Models</h1>
+<table>
+    <tr class="heading">
+        <th>車体名</th>
+        <th>価格</th>
+        <th>燃費</th>
+        <th>乗車定員</th>
+        <th>排気量</th>
+    </tr>
+    <tr>
+        <td class="player-name">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/85188/player1.png" alt="">
+            Compact
+        </td>
+        <td>140万円</td>
+        <td>32km/L</td>
+        <td>4人</td>
+        <td>0.66L</td>
+    </tr>
+    <tr>
+        <td class="player-name">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/85188/player2.png" alt="">
+            Nice player
+        </td>
+        <td>150万円</td>
+        <td>20km/L</td>
+        <td>5人</td>
+        <td>1.3L</td>
+    </tr>
+    <tr>
+        <td class="player-name">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/85188/player3.png" alt="">
+            Cool player
+        </td>
+        <td>290万円</td>
+        <td>37km/L</td>
+        <td>5人</td>
+        <td>1.8L</td>
+    </tr>
+    <tr>
+        <td class="player-name">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/85188/player4.png" alt="">
+            SUV
+        </td>
+        <td>430万円</td>
+        <td>9km/L</td>
+        <td>7人</td>
+        <td>2.7L</td>
+    </tr>
+    <tr>
+        <td class="player-name">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/85188/player5.png" alt="">
+            Minivan
+        </td>
+        <td>290万円</td>
+        <td>15km/L</td>
+        <td>8人</td>
+        <td>1.8L</td>
+    </tr>
+</table>
